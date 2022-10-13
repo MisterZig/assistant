a13308a2873f29557db4c6b377850628474419ed
MisterZig <ziggynewton@gmail.com>
2022-10-13 17:32:31 +0200

    save

--- zig.py
+++ zig.py
@@ -1,3 +1,19 @@
 from pocketsphinx import LiveSpeech
+
+def checkCommands(phrase:str):
+    if "computer set lights" in phrase or "computer lights" in phrase:
+        try:
+            return ("lights", int(phrase[-3:]))
+        except:
+            if phrase[-3:] == "off": # Check other cases
+                return ("lights", 0)
+            elif phrase[-2:] == "on":
+                return ("lights", 100)
+            else:
+                raise(Exception(phrase[-3:]+ "is not a valid int or string"))
+                
+
 for phrase in LiveSpeech():
     print(phrase)
+    if 'computer' in phrase.lower():
+        checkCommands(phrase.lower()) # process commands later
